58fa148a8df280f3cd862a84ca803de6d5a549db
Nicolas Bordon <nbordon91@gmail.com>
2021-12-01 10:30:56 -0300

    agrego captura de coordenadas en la vista

--- src/main/resources/public/js/map.js
+++ src/main/resources/public/js/map.js
@@ -11,14 +11,24 @@ let marker = L.marker([51.505, -0.09]).addTo(myMap)
 myMap.doubleClickZoom.disable()
 myMap.on('dblclick', e => {
     let latLng = myMap.mouseEventToLatLng(e.originalEvent)
-    L.marker([latLng.lat, latLng.lng]).addTo(myMap)
+    myMap.removeLayer(marker)
+    marker = L.marker([latLng.lat, latLng.lng]).addTo(myMap)
+    let lat = document.getElementById("latitud")
+    let lng = document.getElementById("longitud")
+    lat.value = latLng.lat
+    lng.value = latLng.lng
 })
 
 navigator.geolocation.getCurrentPosition(
     (pos) =>{
         const { coords } = pos
         myMap.setView([coords.latitude, coords.longitude ],13)
-        L.marker([coords.latitude, coords.longitude]).addTo(myMap)
+        myMap.removeLayer(marker)
+        marker = L.marker([coords.latitude, coords.longitude]).addTo(myMap)
+        let lat = document.getElementById("latitud")
+        let lng = document.getElementById("longitud")
+        lat.value = coords.latitude
+        lng.value = coords.longitude
     },
     (err) =>{
         console.log(err)
